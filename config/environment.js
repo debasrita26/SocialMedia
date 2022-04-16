b5bcf88b5a10cb0425e308cf55da95c13374173a
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-04-16 23:04:55 +0530

    new committ

--- config/environment.js
+++ config/environment.js
@@ -59,7 +59,7 @@ const production={
     google_client_id: process.env.GOOGLE_CLIENT_ID,
     google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
     google_callback_url: process.env.GOOGLE_CALLBACK_URL,
-    jwt_secret: C5uPaSEp9KAbYdkfD5bI9DYFxupzwFiT,
+    jwt_secret: "C5uPaSEp9KAbYdkfD5bI9DYFxupzwFiT",
     morgan:{
         mode:'combined',
         options: {stream: accessLogStream}
