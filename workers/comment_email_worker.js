1eb2fb3aed04db75e9ef6f2773327b041943bf9a
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-04-18 21:51:33 +0530

    new commit

--- workers/comment_email_worker.js
+++
@@ -1,9 +0,0 @@
-const commentmailer=require('../mailers/comments_mailer');
-
-
-queue.process('emails',function(job,done){
-    console.log('email worker is processing a job',job.data);
-
-    commentmailer.newComment(job.data);
-    done();
-});
