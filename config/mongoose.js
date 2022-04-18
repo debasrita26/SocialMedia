fb239898885a3010ba9fae57ebd810e45dabbe6d
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-04-18 22:12:35 +0530

    new commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const env=require('./environment');
 
+const url="mongodb+srv://debasrita:<password>@cluster0.88isc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect(`mongodb://localhost/social_media_db` , {useNewUrlParser: true });
+mongoose.connect(process.env.SOCIO_DB || url, {useNewUrlParser: true });
 
 const db = mongoose.connection;
 
