140faf670862d44a6f093f73d401ef244fcfe5ec
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-04-21 16:36:05 +0530

    new commit

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.use(session({
     cookie: {              
         maxAge: (1000 * 60 * 100)
     },
-    store: MongoStore.create({mongoUrl: 'mongodb+srv://debasrita:Mongodb12345@cluster0.88isc.mongodb.net/socialmedia_db'})
+    store: MongoStore.create({mongoUrl: process.env.SOCIO_DB})
     // store: new MongoStore(
     //     { 
     //         mongooseConnection: db,
