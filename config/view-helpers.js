6e1802a50a59d975d7afae05cbb00b7c1040eb23
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-04-23 21:18:53 +0530

    changes in the gulpfile

--- config/view-helpers.js
+++ config/view-helpers.js
@@ -6,12 +6,15 @@ const path=require('path');
 module.exports= (app)=>{
     //this will receive the express app instance
     app.locals.assetPath=function(filePath){
+        console.log(filePath)
         //it will check if it is production or development then fetch the correct file
         if(env.name=='development'){
-            return filePath;
+            return "/"+filePath;
         }
         //we need to change the filePath only if it is production 
         //parsing the JSON file so we are reading that file from path.join....
+        console.log( JSON.parse(fs.readFileSync(path.join(__dirname , "../public/assets/rev-manifest.json/")))[filePath])
+
         return '/' + JSON.parse(fs.readFileSync(path.join(__dirname,'../public/assets/rev-manifest.json')))[filePath];
     }
 }
