568de5523883b4f9295b9663edac1f5649e17bb8
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-04-23 21:14:19 +0530

    changes in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -11,14 +11,14 @@ gulp.task('css', function(done){
     gulp.src('./assets/sass/**/*.scss')
     .pipe(sass())
     .pipe(cssnano())
-    .pipe(gulp.dest('/assets.css'));
+    .pipe(gulp.dest('./assets.css'));
 
     gulp.src('./assets/**/*.css')
     .pipe(rev())
     .pipe(gulp.dest('./public/assets'))
     .pipe(
         rev.manifest({
-        base:'./public/assets',
+        cwd:'public',
         merge: true
     })
     )
@@ -27,49 +27,49 @@ gulp.task('css', function(done){
 });
 
 
- gulp.task('js', function(done){
-      console.log('minifying js...');
+//  gulp.task('js', function(done){
+//       console.log('minifying js...');
       
-      gulp.src('./assets/**/*.js')
-      .pipe(uglify())
-      .pipe(rev())
-      .pipe(gulp.dest('./public/assets'))
-      .pipe(rev.manifest({
-          base: './public/assets',
-          merge: true
-      }))
-      .pipe(gulp.dest('./public/assets'));
-      done();
-});
+//       gulp.src('./assets/**/*.js')
+//       .pipe(uglify())
+//       .pipe(rev())
+//       .pipe(gulp.dest('./public/assets'))
+//       .pipe(rev.manifest({
+//           base: './public/assets',
+//           merge: true
+//       }))
+//       .pipe(gulp.dest('./public/assets'));
+//       done();
+// });
 
 
-  gulp.task('images', function(done){
-      console.log('compressing images...');
-     gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
-      //.pipe(imagemin())
-      .pipe(rev())
-      .pipe(gulp.dest('./public/assets'))
-      .pipe(rev.manifest({
-          base: './public/assets',
-          merge: true
-      }))
-      .pipe(gulp.dest('./public/assets'));
-       done();
-  });
+//   gulp.task('images', function(done){
+//       console.log('compressing images...');
+//      gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+//       //.pipe(imagemin())
+//       .pipe(rev())
+//       .pipe(gulp.dest('./public/assets'))
+//       .pipe(rev.manifest({
+//           base: './public/assets',
+//           merge: true
+//       }))
+//       .pipe(gulp.dest('./public/assets'));
+//        done();
+//   });
 
 
- // empty the public/assets directory
-  gulp.task('clean:assets', function(done){
-      del.sync('./public');
-      del.sync("./rev-manifest.json");
-      done();
-  });
+//  // empty the public/assets directory
+//   gulp.task('clean:assets', function(done){
+//       del.sync('./public');
+//       del.sync("./rev-manifest.json");
+//       done();
+//   });
 
-  gulp.task('build', 
-     gulp.series('clean:assets', 'images', 'css', 'js'), 
-     function(done){
-     console.log('Building assets');
-      done();
-  }
-  );
+//   gulp.task('build', 
+//      gulp.series('clean:assets', 'images', 'css', 'js'), 
+//      function(done){
+//      console.log('Building assets');
+//       done();
+//   }
+//   );
  
