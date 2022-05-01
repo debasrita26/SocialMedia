251b4c0e1a63e454d25490f5fd9e9b0ee59c6d2d
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-05-01 14:42:56 +0530

    fixed the chat engine

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.use(mware.setFlash);
 
 // use express router
 app.use('/', require('./routes'));
-app.listen(process.env.PORT|| 8111, function(err){
+app.listen(port, function(err){
     if (err){
         console.log(`Error in running the server: ${err}`);
     }
